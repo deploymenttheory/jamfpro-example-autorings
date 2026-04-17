156a2547f26c6d9dfa0b795b940b38bc85fd3901
Joseph Little <joelittle11@live.com>
2026-04-17 11:37:06 +0100

    parsing script

---
+++ scripts/parsing/parse_shard.py
@@ -0,0 +1,37 @@
+
+import os
+import sys
+import json
+
+FILE_NAME = "shards.json"
+
+if not os.path.exists(FILE_NAME):
+    print(f"error: {FILE_NAME} not found")
+    sys.exit(1)
+
+with open(FILE_NAME, "r", encoding="utf-8") as f:
+    data: dict
+    data = json.load(f)
+
+shards: dict
+shards = data.get("shard", [])
+
+if not shards:
+    print("no shards available")
+    sys.exit(1)
+
+for k, v in shards.items():
+    # HCL identifiers can't have hyphens
+    resource_name = f"auto_{k}".replace("-", "_")
+    group_name    = f"auto-{k}"
+    ids           = ", ".join(str(i) for i in v)
+
+    tf  = f'resource "jamfpro_static_computer_group" "{resource_name}" {{\n'
+    tf += f'  name                  = "{group_name}"\n'
+    tf += f'  assigned_computer_ids = [{ids}]\n'
+    tf += '}\n'
+
+    filename = f"{group_name}.tf"
+    with open(filename, "w", encoding="utf-8") as out:
+        out.write(tf)
+    print(f"wrote {filename}")
